957112d01d01383bc70ad43ff175fe754f5fdc75
H P O M E N <H P O M E N@LAPTOP-OCOVAM4M>
2024-08-06 14:50:20 +0300

    Add project files.

---
+++ ass1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ass1
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            OptimizedBubbleSort(arr);
+            Console.WriteLine("Sorted array: " + string.Join(", ", arr));
+        }
+
+        static void OptimizedBubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                       
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }
+        
+        var intRange = new Range<int>(10, 20);
+            Console.WriteLine("Is 15 in range? " + intRange.IsInRange(15)); 
+            Console.WriteLine("Length of range: " + intRange.Length()); 
+
+            var doubleRange = new Range<double>(1.5, 3.5);
+            Console.WriteLine("Is 2.5 in range? " + doubleRange.IsInRange(2.5)); 
+            Console.WriteLine("Length of range: " + doubleRange.Length()); 
+        }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ ass1/Range.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass1
+{
+
+    public class Range<T> where T : IComparable<T>
+    {
+        private T min;
+        private T max;
+
+  
+        public Range(T minValue, T maxValue)
+        {
+            if (minValue.CompareTo(maxValue) > 0)
+            {
+                throw new ArgumentException("Minimum value must be less than or equal to maximum value.");
+            }
+            min = minValue;
+            max = maxValue;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        }
+
+      
+        public T Length()
+        {
+            dynamic minValue = min;
+            dynamic maxValue = max;
+            return maxValue - minValue;
+        }
+
+
+
+    }
+
+}
